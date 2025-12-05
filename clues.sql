106b795031020e7adfc4e0ca7921e178d3bf656d
RawanAli <rawan.ali.fardan@email.com>
2025-12-05 14:44:03 +0300

    clue completed

--- clues.sql
+++ clues.sql
@@ -55,7 +55,6 @@ JOIN countries AS co
     ON ci.id = co.capital
 WHERE co.name = 'Brazil';
 
-
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
 
@@ -68,6 +67,7 @@ WHERE co.name = 'Brazil';
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
 SELECT name, countrycode
 FROM cities
 WHERE population = 91084;
